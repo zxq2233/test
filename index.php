fdacfd06be7ff14b762e884053e70baf93107efd
zxq2233 <531373473@qq.com>
2017-01-04 21:25:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,130 +1,92 @@
-<?php
+<!DOCTYPE html>
 
-define('PROXY_START', microtime(true));
+<html>
+    <head>
+       <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-require("vendor/autoload.php");
+        <title>头条号视频内容重复源检查</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.2/css/uikit.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.2/js/uikit.js"></script>
 
-use Proxy\Http\Request;
-use Proxy\Http\Response;
-use Proxy\Plugin\AbstractPlugin;
-use Proxy\Event\FilterEvent;
-use Proxy\Config;
-use Proxy\Proxy;
-
-// start the session
-session_start();
-
-// load config...
-Config::load('./config.php');
-
-// custom config file to be written to by a bash script or something
-Config::load('./custom_config.php');
-
-if(!Config::get('app_key')){
-	die("app_key inside config.php cannot be empty!");
+    </head> 
+    <body>
+        <div style="background-color: #222;height: 34px;line-height: 34px; ">
+            
+                
+ <div style="background:url('http://s3.pstatp.com/toutiao/resource/ntoutiao_web/static/image/logo_201f80d.png') center center no-repeat;background-size:auto 80%;height:100%">
+ </div>
+<div class="uk-container uk-container-center">
+    <div class="uk-grid" data-uk-grid-margin>
+        <?php
+        function get_data($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);   
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+        if(isset($_POST["turl"])){
+            $returned_content = get_data($_POST["turl"]);
+preg_match_all('/group_id:\'(.*?)\',/',$returned_content,$p);
+preg_match_all('/item_id: \'(.*?)\'/',$returned_content,$p1);
+if($p[1][0]==$p1[1][0]){
+    $msg="您的视频未发现重复！";
+}else {
+    $u='http://toutiao.com/group/'.$p[1][0].'/';
+   $msg='您的视频与<a href="'.$u.'" target="_blank">'.$u.'</a>重复';
 }
 
-if(!function_exists('curl_version')){
-	die("cURL extension is not loaded!");
+
+echo '<div class="uk-width-1-1 uk-margin-top">'.$msg.'</div>';
+ 
+}else 
+{ 
+echo'<div class="uk-width-large-3-4 uk-width-small-1-1 uk-width-medium-3-4 uk-margin-top">
+<form  method="post">
+        <input type="text" name="turl" placeholder="请输入文章链接" class="uk-form-large uk-width-1-1" style="border-color: #8ec73b!important;
+    background: #fafff2!important;
+    color: #659f13!important;border-radius: 4px;" >
+    </div>
+    <div class="uk-width-large-1-4 uk-width-small-1-1 uk-width-medium-1-4 uk-margin-top">
+        
+        <input type="submit" class="uk-button uk-button-success uk-button-large uk-width-1-1" value="检测">
+        </form>
+       
+    </div>';
 }
+      ?>  
+        
+       <div class="uk-width-1-1 uk-margin-top">
+        
+    <div class="uk-width-medium-1-1">
+        <div class="uk-panel uk-panel-box uk-panel-box-primary">开发未完成！</div>
+    </div>
+           
+           
+       </div>' 
+        
+        
+        
+        
+    
+</div>
 
-// how are our URLs be generated from this point? this must be set here so the proxify_url function below can make use of it
-if(Config::get('url_mode') == 1){
-	Config::set('encryption_key', md5(Config::get('app_key').$_SERVER['REMOTE_ADDR']));
-} else if(Config::get('url_mode') == 2){
-	Config::set('encryption_key', md5(Config::get('app_key').session_id()));
-}
-
-// very important!!! otherwise requests are queued while waiting for session file to be unlocked
-session_write_close();
-
-// form submit in progress...
-if(isset($_POST['url'])){
-	
-	$url = $_POST['url'];
-	$url = add_http($url);
-	
-	header("HTTP/1.1 302 Found");
-	header('Location: '.proxify_url($url));
-	exit;
-	
-} else if(!isset($_GET['q'])){
-
-	// must be at homepage - should we redirect somewhere else?
-	if(Config::get('index_redirect')){
-		
-		// redirect to...
-		header("HTTP/1.1 302 Found"); 
-		header("Location: ".Config::get('index_redirect'));
-		
-	} else {
-		echo render_template("./templates/main.php", array('version' => Proxy::VERSION));
-	}
-
-	exit;
-}
-
-// decode q parameter to get the real URL
-$url = base64_decrypt($_GET['q']);
-
-$proxy = new Proxy();
-
-// load plugins
-foreach(Config::get('plugins', array()) as $plugin){
-
-	$plugin_class = $plugin.'Plugin';
-	
-	if(file_exists('./plugins/'.$plugin_class.'.php')){
-	
-		// use user plugin from /plugins/
-		require_once('./plugins/'.$plugin_class.'.php');
-		
-	} else if(class_exists('\\Proxy\\Plugin\\'.$plugin_class)){
-	
-		// does the native plugin from php-proxy package with such name exist?
-		$plugin_class = '\\Proxy\\Plugin\\'.$plugin_class;
-	}
-	
-	// otherwise plugin_class better be loaded already through composer.json and match namespace exactly \\Vendor\\Plugin\\SuperPlugin
-	$proxy->getEventDispatcher()->addSubscriber(new $plugin_class());
-}
-
-try {
-
-	// request sent to index.php
-	$request = Request::createFromGlobals();
-	
-	// remove all GET parameters such as ?q=
-	$request->get->clear();
-	
-	// forward it to some other URL
-	$response = $proxy->forward($request, $url);
-	
-	// if that was a streaming response, then everything was already sent and script will be killed before it even reaches this line
-	$response->send();
-	
-} catch (Exception $ex){
-
-	// if the site is on server2.proxy.com then you may wish to redirect it back to proxy.com
-	if(Config::get("error_redirect")){
-	
-		$url = render_string(Config::get("error_redirect"), array(
-			'error_msg' => rawurlencode($ex->getMessage())
-		));
-		
-		// Cannot modify header information - headers already sent
-		header("HTTP/1.1 302 Found");
-		header("Location: {$url}");
-		
-	} else {
-	
-		echo render_template("./templates/main.php", array(
-			'url' => $url,
-			'error_msg' => $ex->getMessage(),
-			'version' => Proxy::VERSION
-		));
-		
-	}
-}
-
-?>
+</div>
+                
+               
+                
+                
+                
+        </div>
+                
+                
+        
+            
+    </body>
+    
+</html>
